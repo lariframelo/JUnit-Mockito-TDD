625ddd8434bca3ccba5f638e0d6adac03969e41e
Larissa Fraga Melo <84105505+lariframelo@users.noreply.github.com>
2021-12-01 16:14:48 -0300

    Organização de arquivos de teste

--- JUnit/TestesUnitarios+-+inicio/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ JUnit/TestesUnitarios+-+inicio/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -7,9 +7,6 @@ import java.util.Date;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.utils.DataUtils;
-import org.junit.Assert;
-import org.junit.jupiter.api.Test;
 
 public class LocacaoService {
 
@@ -30,19 +27,5 @@ public class LocacaoService {
 
         return locacao;
     }
-    @Test
-    public void teste() {
-        //cenario
-        LocacaoService service = new LocacaoService();
-        Usuario usuario = new Usuario("Usuario 1");
-        Filme filme = new Filme("Filme 1", 2, 5.0);
 
-        //ação
-        Locacao locacao = service.alugarFilme(usuario, filme);
-
-        //verificação
-		Assert.assertTrue(locacao.getValor() == 5.0);
-        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(),new Date()));
-        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(),DataUtils.obterDataComDiferencaDias(1)));
-    }
 }
---
+++ JUnit/TestesUnitarios+-+inicio/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -0,0 +1,28 @@
+package br.ce.wcaquino.servicos;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+public class LocacaoServiceTest {
+    @Test
+    public void teste() {
+        //cenario
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuario 1");
+        Filme filme = new Filme("Filme 1", 2, 5.0);
+
+        //ação
+        Locacao locacao = service.alugarFilme(usuario, filme);
+
+        //verificação
+        Assert.assertTrue(locacao.getValor() == 5.0);
+        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(),new Date()));
+        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(),DataUtils.obterDataComDiferencaDias(1)));
+    }
+}
