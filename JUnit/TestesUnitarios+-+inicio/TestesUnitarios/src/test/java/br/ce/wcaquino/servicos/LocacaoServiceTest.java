5dc01644f948f6353d7e79000d528a67bac59cd5
Larissa Fraga Melo <84105505+lariframelo@users.noreply.github.com>
2021-12-03 10:03:34 -0300

    order

--- JUnit/TestesUnitarios+-+inicio/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ JUnit/TestesUnitarios+-+inicio/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -47,7 +47,7 @@ public class LocacaoServiceTest {
         LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Usuario 1");
         Filme filme = new Filme("Filme 1", 1, 5.0);
-        
+
         //acao
         Locacao locacao = service.alugarFilme(usuario, filme);
 
